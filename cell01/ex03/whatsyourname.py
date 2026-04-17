4ceadab21f8a230c20ada97be9f4bdeafc02f19f
Axel Lee <axellee1994@gmail.com>
2026-04-17 11:16:38 +0800

    Removed 42 Headers

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,15 +1,3 @@
-#******************************************************************************#
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    whatsyourname.py                                   :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: axlee <axlee@student.42.fr>                +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/04/17 11:05:05 by axlee             #+#    #+#              #
-#    Updated: 2026/04/17 11:07:35 by axlee            ###   ########.fr        #
-#                                                                              #
-#******************************************************************************#
-
 first_name = input("Hey, what's your first name? : ")
 last_name = input("And your last name? : ")
 whole_name = first_name + " " + last_name
